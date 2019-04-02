a87e169ee91a33d712b6ca552076889f61fb069d
Philip Boardman <dustykeyboard@brd.mn>
2019-04-02 20:45:49 +1100

    Separate slides for examples

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -1,4 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const buttonStyle = `
+  font-size: 48px;
+  color: white;
+  background: purple;
+  padding: 0.5em;
+  border: 0;
+  border-radius: 0.25em;
+`;
 
 const StyledButton = styled.button`
   font-size: 1em;
--- src/components/ThemedButton.jsx
+++ src/components/ThemedButton.jsx
@@ -1,26 +1,9 @@
 import styled from 'styled-components';
+import StyledButton from './StyledButton';
 
-const ThemedButton = styled.button`
-  font-size: 48px;
+const ThemedButton = styled(StyledButton)`
   color: ${p => p.theme.colors.label};
   background: ${p => p.theme.colors.brand};
-  padding: 0.5em;
-  border: 0;
-  border-radius: 0.25em;
-  cursor: pointer;
-  position: relative;
-  transition: 0.2s;
-  -webkit-appearance: none;
-  outline: none;
-
-  &:hover {
-    top: -0.1rem;
-    box-shadow: 0 0.2em 0.5em rgba(127, 127, 127, 0.5);
-  }
-  &:active {
-    top: 0;
-    box-shadow: 0 0.1em 0.3em rgba(127, 127, 127, 0.5);
-  }
 `;
 
 export default ThemedButton;
