a888ce726f7fa8cc118dc195d248956ef9139f68
Philip Boardman <dustykeyboard@brd.mn>
2019-04-03 14:32:23 +1100

    Consistent colours

--- src/components/StyledButton.jsx
+++ src/components/StyledButton.jsx
@@ -2,8 +2,8 @@ import styled, { css } from 'styled-components';
 
 export const buttonStyle = css`
   font-size: 48px;
-  color: white;
-  background: purple;
+  color: #f8f0f8;
+  background: #8a24c2;
   padding: 0.5em;
   border: 0;
   border-radius: 0.25em;
